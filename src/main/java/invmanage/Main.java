794b5ea562ddac084001135090a289a9409659a9
Ellis Crowther <ehc1999@gmail.com>
2023-03-21 11:39:44 +0000

    feature(core): Converted project to use javafx and started work on main function

---
+++ src/main/java/invmanage/Main.java
@@ -0,0 +1,16 @@
+package invmanage;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        // get path to login file
+        // load file
+        // Load scene
+        // Show scene
+    }
+
+    // Does this class need a main?
+}
