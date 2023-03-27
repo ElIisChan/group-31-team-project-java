593cb50994db7732f803fa350c0e74ef4aac4a2f
Ellis Crowther <ehc1999@gmail.com>
2023-03-27 20:40:01 +0100

    chore(core): Start of moving data into a table

--- src/main/java/com/example/inventory/DashboardController.java
+++ src/main/java/com/example/inventory/DashboardController.java
@@ -3,12 +3,14 @@ package com.example.inventory;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
 import java.io.IOException;
+import java.sql.*;
 
 public class DashboardController {
 
@@ -19,21 +21,48 @@ public class DashboardController {
     @FXML private VBox notifPane;
 
 
-    public void initialize() {
+    public void initialize() throws ClassNotFoundException, SQLException {
         showDashboard();
         btn_logout.setOnAction(e -> handleLogout());
         btn_orders.setOnAction(e -> showOrders());
     }
 
     // Handle dashboard button
-    public void showDashboard() {
+    public void showDashboard() throws ClassNotFoundException, SQLException {
         // Hide other panes
         orderPane.setVisible(false);
         notifPane.setVisible(false);
         // Show dashboard pane
         dashPane.setVisible(true);
-
         // Setup db locally, then connect to db and get data
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection dbConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/aston_31?user=a31&password=a31");
+        // Get data from product table
+        String query = "SELECT * FROM products";
+        ResultSet resultSet;
+        try (PreparedStatement statement = dbConnection.prepareStatement(query)) {
+            statement.executeQuery();
+            resultSet = statement.getResultSet();
+            // If we get data from the database, add them to a table
+            while (resultSet.next()) {
+                // Get data from resultset
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("product_name");
+                int quantity = resultSet.getInt("quantity");
+                double price = resultSet.getDouble("price");
+                // Create a label for each piece of data
+                Label idLabel = new Label(Integer.toString(id));
+                Label nameLabel = new Label(name);
+                Label quantityLabel = new Label(Integer.toString(quantity));
+                Label priceLabel = new Label(Double.toString(price));
+                // Add labels to VBox
+                dashPane.getChildren().add(idLabel);
+                dashPane.getChildren().add(nameLabel);
+                dashPane.getChildren().add(quantityLabel);
+                dashPane.getChildren().add(priceLabel);
+            }
+        }
+
     }
 
     // Handle logout button
